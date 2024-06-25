2ab580b7b09cf6d5eca5b1cb60f16f6478a7c926
Aman Singh <aman.singh@tatvasoft.com>
2024-06-25 12:36:56 +0530

    inital commit

---
+++ MAUIWorkSpace/MAUIWorkSpace/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIWorkSpace
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
